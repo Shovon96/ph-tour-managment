05f57d745558b87c5e6c86b8b03a3f811961772c
Shovon96 <fakhruddinshovon@gmail.com>
2025-07-23 04:14:29 +0600

    send response code do not repetation

--- PH-Tour-mng-server/src/app/modules/users/user.controller.ts
+++ PH-Tour-mng-server/src/app/modules/users/user.controller.ts
@@ -1,25 +1,32 @@
 import { NextFunction, Request, Response } from "express";
 import statusCode from 'http-status-codes'
 import { UserServices } from "./user.service";
-import AppError from "../../errorHalpers/AppError";
+import { catchAsync } from "../../utils/catchAsync";
+import { sendResponse } from "../../utils/sendResponse";
 
-const createUser = async (req: Request, res: Response, next: NextFunction) => {
-        const user = await UserServices.createUser(req.body);
-        res.status(statusCode.CREATED).json({
-            success: true,
-            message: "User created successfully",
-            user
-        })
-}
+const createUser = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    const user = await UserServices.createUser(req.body);
 
-const getAllUsers = async (req: Request, res: Response) => {
-    const users = await UserServices.getAllUsers()
-    res.status(statusCode.OK).json({
+    sendResponse(res, {
+        statusCode: statusCode.CREATED,
         success: true,
-        message: "Get all users successfully",
-        data: users
+        message: "User Created Successfully",
+        data: user,
     })
-}
+})
+
+const getAllUsers = catchAsync(async (req: Request, res: Response) => {
+    const result = await UserServices.getAllUsers()
+    console.log(result.meta)
+
+    sendResponse(res, {
+        success: true,
+        statusCode: statusCode.CREATED,
+        message: "All Users Retrieved Successfully",
+        data: result.data,
+        meta: result.meta
+    })
+})
 
 export const UserControllers = {
     createUser,
--- PH-Tour-mng-server/src/app/modules/users/user.service.ts
+++ PH-Tour-mng-server/src/app/modules/users/user.service.ts
@@ -9,7 +9,11 @@ const createUser = async (payload: Partial<IUser>) => {
 
 const getAllUsers = async () => {
     const users = await User.find({})
-    return users
+    const totalUser = await User.countDocuments()
+    return {
+        data: users,
+        meta: { total: totalUser }
+    }
 }
 
 export const UserServices = {
