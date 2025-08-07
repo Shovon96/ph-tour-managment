6e7575e47c00ecf9f51125241e98c2593aa238cb
Shovon96 <fakhruddinshovon@gmail.com>
2025-08-07 20:56:56 +0600

    page, limit, skip for tour query

--- PH-Tour-mng-server/src/app/modules/tour/tour.service.ts
+++ PH-Tour-mng-server/src/app/modules/tour/tour.service.ts
@@ -52,23 +52,38 @@ const createTour = async (payload: ITour) => {
 const getAllTours = async (query: Record<string, string>) => {
     const filter = query
     const searchTerm = query.searchTerm || ""
-    
-    delete filter["searchTerm"]
+    const sort = query.sort || "-createdAt"
+    const fields = query.fields?.split(",").join(" ") || ""
+    const page = Number(query.page) || 1
+    const limit = Number(query.limit) || 10
+    const skip = (page - 1) * limit
+
+    const excludedFields = ["searchTerm", "fields", "sort", "limit", "page"]
+    for (const field of excludedFields) {
+        delete filter[field]
+    }
+
 
     const tourSerchFields = ["title", "description", "location"]
     const searchQuery = {
         $or: tourSerchFields.map(field => ({ [field]: { $regex: searchTerm, $options: 'i' } }))
     }
 
-    const tours = await Tour.find(searchQuery).find(filter)
+    const tours = await Tour.find(searchQuery).find(filter).sort(sort).select(fields).skip(skip).limit(limit)
 
     const totalTours = await Tour.countDocuments()
+    const totalPage = Math.ceil(totalTours / limit)
+
+    const metaData = {
+        page,
+        limit,
+        totalTours,
+        totalPage
+    }
 
     return {
         data: tours,
-        meta: {
-            total: totalTours
-        }
+        meta: metaData
     }
 }
 
