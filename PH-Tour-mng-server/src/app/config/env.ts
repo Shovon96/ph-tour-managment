69e424b9a21f1177ee15dad094308db783120f4e
Shovon96 <fakhruddinshovon@gmail.com>
2025-08-10 21:17:04 +0600

    create sslCommerz env variables and interface sslPayment

--- PH-Tour-mng-server/src/app/config/env.ts
+++ PH-Tour-mng-server/src/app/config/env.ts
@@ -12,16 +12,54 @@ interface EnvConfig {
     JWT_REFRESH_EXPIRES: string,
     BCRYPT_SALT_ROUND: string,
     SUPER_ADMIN_EMAIL: string,
-    SUPER_ADMIN_PASSWORD: string
-    GOOGLE_CLIENT_SECRET: string
-    GOOGLE_CLIENT_ID: string
-    GOOGLE_CALLBACK_URL: string
-    EXPRESS_SESSION_SECRET: string
+    SUPER_ADMIN_PASSWORD: string,
+    GOOGLE_CLIENT_SECRET: string,
+    GOOGLE_CLIENT_ID: string,
+    GOOGLE_CALLBACK_URL: string,
+    EXPRESS_SESSION_SECRET: string,
     FRONTEND_URL: string
+    SSL: {
+        SSL_STORE_ID: string
+        SSL_STORE_PASS: string
+        SSL_PAYMENT_API: string
+        SSL_VALIDATION_API: string
+        SSL_SUCCESS_FRONTEND_URL: string,
+        SSL_FAIL_FRONTEND_URL: string,
+        SSL_CANCEL_FRONTEND_URL: string,
+        SSL_SUCCESS_BACKEND_URL: string,
+        SSL_FAIL_BACKEND_URL: string,
+        SSL_CANCEL_BACKEND_URL: string
+    }
 }
 
 const loadEnvVariables = (): EnvConfig => {
-    const requiredEnvVariable: string[] = ['PORT', 'DB_URL', 'NODE_ENV', 'JWT_ACCESS_TOKEN', 'JWT_TOKEN_EXPIRES', 'BCRYPT_SALT_ROUND', 'SUPER_ADMIN_EMAIL', 'SUPER_ADMIN_PASSWORD', 'JWT_REFRESH_TOKEN', 'JWT_REFRESH_EXPIRES', 'GOOGLE_CLIENT_SECRET', 'GOOGLE_CLIENT_ID', 'GOOGLE_CALLBACK_URL', 'EXPRESS_SESSION_SECRET', 'FRONTEND_URL',]
+    const requiredEnvVariable: string[] = [
+        'PORT',
+        'DB_URL',
+        'NODE_ENV',
+        'JWT_ACCESS_TOKEN',
+        'JWT_TOKEN_EXPIRES',
+        'BCRYPT_SALT_ROUND',
+        'SUPER_ADMIN_EMAIL',
+        'SUPER_ADMIN_PASSWORD',
+        'JWT_REFRESH_TOKEN',
+        'JWT_REFRESH_EXPIRES',
+        'GOOGLE_CLIENT_SECRET',
+        'GOOGLE_CLIENT_ID',
+        'GOOGLE_CALLBACK_URL',
+        'EXPRESS_SESSION_SECRET',
+        'FRONTEND_URL',
+        'SSL_STORE_ID',
+        'SSL_STORE_PASS',
+        'SSL_PAYMENT_API',
+        'SSL_VALIDATION_API',
+        "SSL_SUCCESS_FRONTEND_URL",
+        "SSL_FAIL_FRONTEND_URL",
+        "SSL_CANCEL_FRONTEND_URL",
+        "SSL_SUCCESS_BACKEND_URL",
+        "SSL_FAIL_BACKEND_URL",
+        "SSL_CANCEL_BACKEND_URL"
+    ]
     requiredEnvVariable.forEach(key => {
         if (!process.env[key]) {
             throw new Error(`Missing required env variable ${key}`)
@@ -44,6 +82,19 @@ const loadEnvVariables = (): EnvConfig => {
         GOOGLE_CALLBACK_URL: process.env.GOOGLE_CALLBACK_URL as string,
         EXPRESS_SESSION_SECRET: process.env.EXPRESS_SESSION_SECRET as string,
         FRONTEND_URL: process.env.FRONTEND_URL as string,
+        // SSL
+        SSL: {
+            SSL_STORE_ID: process.env.SSL_STORE_ID as string,
+            SSL_STORE_PASS: process.env.SSL_STORE_PASS as string,
+            SSL_PAYMENT_API: process.env.SSL_PAYMENT_API as string,
+            SSL_VALIDATION_API: process.env.SSL_VALIDATION_API as string,
+            SSL_SUCCESS_FRONTEND_URL: process.env.SSL_SUCCESS_FRONTEND_URL as string,
+            SSL_FAIL_FRONTEND_URL: process.env.SSL_FAIL_FRONTEND_URL as string,
+            SSL_CANCEL_FRONTEND_URL: process.env.SSL_CANCEL_FRONTEND_URL as string,
+            SSL_SUCCESS_BACKEND_URL: process.env.SSL_SUCCESS_BACKEND_URL as string,
+            SSL_FAIL_BACKEND_URL: process.env.SSL_FAIL_BACKEND_URL as string,
+            SSL_CANCEL_BACKEND_URL: process.env.SSL_CANCEL_BACKEND_URL as string
+        }
     }
 
 }
