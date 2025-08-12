e5cf43824868434ba1f360db0e3d10757afccec2
Shovon96 <fakhruddinshovon@gmail.com>
2025-08-12 13:27:53 +0600

    Configure Multer and Cloudinary For Image Upload

--- PH-Tour-mng-server/src/app/config/env.ts
+++ PH-Tour-mng-server/src/app/config/env.ts
@@ -30,6 +30,11 @@ interface EnvConfig {
         SSL_FAIL_BACKEND_URL: string,
         SSL_CANCEL_BACKEND_URL: string
     }
+    CLOUDINARY: {
+        CLOUDINARY_CLOUD_NAME: string
+        CLOUDINARY_API_KEY: string
+        CLOUDINARY_API_SECRET: string
+    }
 }
 
 const loadEnvVariables = (): EnvConfig => {
@@ -58,7 +63,10 @@ const loadEnvVariables = (): EnvConfig => {
         "SSL_CANCEL_FRONTEND_URL",
         "SSL_SUCCESS_BACKEND_URL",
         "SSL_FAIL_BACKEND_URL",
-        "SSL_CANCEL_BACKEND_URL"
+        "SSL_CANCEL_BACKEND_URL",
+        "CLOUDINARY_CLOUD_NAME",
+        "CLOUDINARY_API_KEY",
+        "CLOUDINARY_API_SECRET"
     ]
     requiredEnvVariable.forEach(key => {
         if (!process.env[key]) {
@@ -94,6 +102,11 @@ const loadEnvVariables = (): EnvConfig => {
             SSL_SUCCESS_BACKEND_URL: process.env.SSL_SUCCESS_BACKEND_URL as string,
             SSL_FAIL_BACKEND_URL: process.env.SSL_FAIL_BACKEND_URL as string,
             SSL_CANCEL_BACKEND_URL: process.env.SSL_CANCEL_BACKEND_URL as string
+        },
+        CLOUDINARY: {
+            CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME as string,
+            CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY as string,
+            CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET as string
         }
     }
 
