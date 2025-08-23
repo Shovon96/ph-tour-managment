e752c4251652a27d45be11d468a5b8846e35b818
Shovon96 <fakhruddinshovon@gmail.com>
2025-08-23 18:22:01 +0600

    vite installed with react, ts and setup the project with shadcn with tailwind css

---
+++ ph-tour-mng-frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <div className='p-4 justify-center items-center flex flex-col'>
+      <h1 className='text-2xl font-bold'>Hello Tour project ui with shadcn</h1>
+      <Button>Click Me</Button>
+    </div>
+  )
+}
+
+export default App
